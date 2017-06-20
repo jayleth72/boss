932ed67e9b1c81c0035124fde59da1cfb0bc9f6a
Jay Letheby <jay.letheby@gmail.com>
2017-06-21 07:34:26 +1000

    added spiner and working

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { EMAIL_CHANGED,
          PASSWORD_CHANGED,
          LOGIN_USER_SUCCESS,
-         LOGIN_USER_FAIL
+         LOGIN_USER_FAIL,
+         LOGIN_USER
        } from './types';
 
 export const emailChanged = (text) => {
@@ -21,12 +22,16 @@ export const passwordChanged = (text) => {
 
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(user => loginUserSuccess(dispatch, user))
-    .catch(() => {
-      firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(user => loginUserSuccess(dispatch, user))
-        .catch(() => loginUserFail(dispatch));
+    dispatch ({ type: LOGIN_USER} );
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch((error) => {
+        console.log(error);
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
       });
   };
 };
